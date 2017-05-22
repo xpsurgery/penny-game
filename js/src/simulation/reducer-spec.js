4822a684902f1642919470f184ff08eb3adeec91
Kevin Rutherford <kevin@rutherford-software.com>
2017-05-22 12:26:34 +0100

    Add tests for the reducer helper functions

--- js/src/simulation/reducer-spec.js
+++ js/src/simulation/reducer-spec.js
@@ -1,32 +1,32 @@
 import { expect } from 'chai'
 import { reductio } from '../app/specHelper'
 import { tick } from './actionCreators'
-import reducer from './reducer'
+import reducer, { workInProgress, valueDelivered } from './reducer'
 
 describe('productionLine', () => {
 
   let testCases = [
-    { ticks: 0, state: {
+    { ticks: 0, wip: 0, value: 0, state: {
       s1: {in: [], wip: [], out: []},
       s2: {in: [], wip: [], out: []},
       s3: {in: [], wip: [], out: []},
       s4: {in: [], wip: [], out: []}}
-    }, { ticks: 1, state: {
+    }, { ticks: 1, wip: 5, value: 0, state: {
       s1: {in: ['H', 'H', 'H', 'H', 'H'], wip: [], out: []},
       s2: {in: [], wip: [], out: []},
       s3: {in: [], wip: [], out: []},
       s4: {in: [], wip: [], out: []}}
-    }, { ticks: 2, state: {
+    }, { ticks: 2, wip: 5, value: 0, state: {
       s1: {in: ['H', 'H', 'H', 'H'], wip: ['H'], out: []},
       s2: {in: [], wip: [], out: []},
       s3: {in: [], wip: [], out: []},
       s4: {in: [], wip: [], out: []}}
-    }, { ticks: 3, state: {
+    }, { ticks: 3, wip: 5, value: 0, state: {
       s1: {in: ['H', 'H', 'H', 'H'], wip: ['T'], out: []},
       s2: {in: [], wip: [], out: []},
       s3: {in: [], wip: [], out: []},
       s4: {in: [], wip: [], out: []}}
-    }, { ticks: 4, state: {
+    }, { ticks: 4, wip: 5, value: 0, state: {
       s1: {in: ['H', 'H', 'H', 'H'], wip: [], out: ['T']},
       s2: {in: [], wip: [], out: []},
       s3: {in: [], wip: [], out: []},
@@ -35,10 +35,25 @@ describe('productionLine', () => {
   ]
 
   testCases.forEach(ex => {
-    it('works for ${ex.ticks} ticks', () => {
-      let actions = Array(ex.ticks).fill(tick())
-      let state = reductio(reducer, actions)
-      expect(state.batchesOf5).to.deep.equal(ex.state)
+    describe(`after ${ex.ticks} ticks`, () => {
+      var state
+
+      beforeEach(() => {
+        let actions = Array(ex.ticks).fill(tick())
+        state = reductio(reducer, actions)
+      })
+
+      it('all coins and workers are in the correct state', () => {
+        expect(state.batchesOf5).to.deep.equal(ex.state)
+      })
+
+      it('calculates the current WIP', () => {
+        expect(workInProgress(state.batchesOf5)).to.deep.equal(ex.wip)
+      })
+
+      it('calculates the total value delivered', () => {
+        expect(valueDelivered(state.batchesOf5)).to.deep.equal(ex.value)
+      })
     })
   })
 
