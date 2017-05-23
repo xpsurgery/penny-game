31404d4fb4aa55c245768df9b3c20ef7c274450b
Kevin Rutherford <kevin@rutherford-software.com>
2017-05-23 16:30:35 +0100

    Fix the stats

--- js/src/simulation/ProductionLine.js
+++ js/src/simulation/ProductionLine.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import Worker from './Worker'
 import Stats from './Stats'
+import { coins } from './reducer'
 
 export default ({ line }) =>
   <div className='production-line group'>
-    <Worker {...line.s1} />
-    <Worker {...line.s2} />
-    <Worker {...line.s3} />
-    <Worker {...line.s4} />
+    <Worker {...coins(line.s1)} />
+    <Worker {...coins(line.s2)} />
+    <Worker {...coins(line.s3)} />
+    <Worker {...coins(line.s4)} />
     <Stats line={line} />
   </div>
 
--- js/src/simulation/Simulation.js
+++ js/src/simulation/Simulation.js
@@ -3,7 +3,6 @@ import { connect } from 'react-redux'
 import Repeat from '../repeat/Repeat'
 import { tick } from '../repeat/actionCreators'
 import ProductionLine from './ProductionLine'
-import { coins } from './reducer'
 
 const Simulation = ({ batchesOf20, batchesOf5, slowDev }) =>
   <div className='simulation'>
@@ -19,26 +18,7 @@ const Simulation = ({ batchesOf20, batchesOf5, slowDev }) =>
     </p>
   </div>
 
-const mapStateToProps = ({ simulation }) => ({
-  batchesOf20: {
-    s1: coins(simulation.batchesOf20.s1),
-    s2: coins(simulation.batchesOf20.s2),
-    s3: coins(simulation.batchesOf20.s3),
-    s4: coins(simulation.batchesOf20.s4)
-  },
-  batchesOf5: {
-    s1: coins(simulation.batchesOf5.s1),
-    s2: coins(simulation.batchesOf5.s2),
-    s3: coins(simulation.batchesOf5.s3),
-    s4: coins(simulation.batchesOf5.s4)
-  },
-  slowDev: {
-    s1: coins(simulation.slowDev.s1),
-    s2: coins(simulation.slowDev.s2),
-    s3: coins(simulation.slowDev.s3),
-    s4: coins(simulation.slowDev.s4)
-  }
-})
+const mapStateToProps = ({ simulation }) => simulation
 
 export default connect(mapStateToProps)(Simulation)
 
