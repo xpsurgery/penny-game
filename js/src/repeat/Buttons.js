f08687eefdebb71e828ce748a59895af23cb9597
Kevin Rutherford <kevin@rutherford-software.com>
2017-05-24 10:56:19 +0100

    Display the time remaining in the game

--- js/src/repeat/Buttons.js
+++ js/src/repeat/Buttons.js
@@ -2,13 +2,20 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { enableRepeater, disableRepeater } from './actionCreators'
 
-const Buttons = ({ enableRepeater, disableRepeater }) =>
-  <div className='buttons'>
+const Buttons = ({ remaining, enableRepeater, disableRepeater }) =>
+  <div className='play-controls'>
     <button onClick={() => enableRepeater()}> play </button>
     <button onClick={() => disableRepeater()}> pause </button>
+    <div className='time-remaining'>
+      Time remaining: {remaining} seconds
+    </div>
   </div>
 
-export default connect( null, {
+const mapStateToProps = ({ repeat }) => ({
+  remaining: 240 - repeat.ticksSoFar
+})
+
+export default connect(mapStateToProps, {
   enableRepeater,
   disableRepeater
 })(Buttons)
