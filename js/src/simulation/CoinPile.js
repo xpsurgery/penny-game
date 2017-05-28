5c1e05948dd854bb8f80d18cfd11f8d0c8ce6b4f
Kevin Rutherford <kevin@rutherford-software.com>
2017-05-28 20:44:06 +0100

    Display coins instead of H/T

--- js/src/simulation/CoinPile.js
+++ js/src/simulation/CoinPile.js
@@ -1,10 +1,24 @@
 import React from 'react'
 
+const Heads = () =>
+  <svg viewPort='0 0 200 200'>
+    <circle cx='6' cy='6' r='6' fill='#daa520' />
+  </svg>
+
+const Tails = () =>
+  <svg viewPort='0 0 200 200'>
+    <circle cx='6' cy='6' r='6' fill='silver' />
+  </svg>
+
 export default ({ coins }) =>
   <div className='coins'>
     {
       coins.map((coin, i) => (
-        <div className='coin' key={i}>{coin}</div>
+        <div className='coin' key={i}>
+          {
+            (coin === 'H') ? <Heads /> : <Tails />
+          }
+        </div>
       ))
     }
   </div>
