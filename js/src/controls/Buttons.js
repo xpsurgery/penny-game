45054764c2041561fa89d7ca788ac412d46387be
Kevin Rutherford <kevin@rutherford-software.com>
2017-06-04 18:58:31 +0100

    Allow the game to run forever

--- js/src/controls/Buttons.js
+++ js/src/controls/Buttons.js
@@ -2,13 +2,10 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { enableRepeater, disableRepeater } from './actionCreators'
 
-const Buttons = ({ ticksRemaining, enableRepeater, disableRepeater }) =>
+const Buttons = ({ enableRepeater, disableRepeater }) =>
   <div className='play-controls'>
     <button onClick={() => enableRepeater()}> play </button>
     <button onClick={() => disableRepeater()}> pause </button>
-    <div className='time-remaining'>
-      Time remaining: {ticksRemaining} ticks
-    </div>
   </div>
 
 const mapStateToProps = ({ controls }) => controls
--- js/src/controls/reducer.js
+++ js/src/controls/reducer.js
@@ -1,18 +1,11 @@
-import { TICK, ENABLE_REPEATER, DISABLE_REPEATER } from './actionCreators'
+import { ENABLE_REPEATER, DISABLE_REPEATER } from './actionCreators'
 
 const initialState = {
-  ticksRemaining: 300,
   enabled: false
 }
 
 export default (state=initialState, action) => {
   switch (action.type) {
-
-    case TICK:
-      return {
-        ...state,
-        ticksRemaining: state.ticksRemaining - 1
-      }
 
     case ENABLE_REPEATER:
       return {
