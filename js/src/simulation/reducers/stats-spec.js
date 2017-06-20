78b5804c44429d0d2b1cd6d3171312f907eb98bd
Kevin Rutherford <kevin@rutherford-software.com>
2017-06-20 18:20:18 +0100

    Fix time-to-first-value stats

--- js/src/simulation/reducers/stats-spec.js
+++ js/src/simulation/reducers/stats-spec.js
@@ -1,7 +1,7 @@
 import { expect } from 'chai'
 import { reductio } from '../../app/specHelper'
 import { tick } from '../../controls/actionCreators'
-import { deliverBatch } from '../actionCreators'
+import { receiveBatch } from '../actionCreators'
 import stats, { ticksToFirstValue } from './stats'
 
 describe('ticks remaining', () => {
@@ -9,12 +9,12 @@ describe('ticks remaining', () => {
   let reducer = stats('agile')
 
   it('ignores other simulations', () => {
-    state = reducer(state, deliverBatch())
+    state = reducer(state, receiveBatch())
     expect(ticksToFirstValue(state)).to.equal(undefined)
   })
 
   it('counts the ticks upto delivery', () => {
-    let actions = [tick(), tick(), deliverBatch('agile', 's4', [])]
+    let actions = [tick(), tick(), receiveBatch('agile', 'customer', [])]
     state = reductio(reducer, actions)
     expect(ticksToFirstValue(state)).to.equal(2)
   })
@@ -22,9 +22,9 @@ describe('ticks remaining', () => {
   it("doesn't change after the first delivery", () => {
     let actions = [
       tick(), tick(), tick(),
-      deliverBatch('agile', 's4', []),
+      receiveBatch('agile', 'customer', []),
       tick(), tick(), tick(),
-      deliverBatch('agile', 's4', [])
+      receiveBatch('agile', 'customer', [])
     ]
     state = reductio(reducer, actions)
     expect(ticksToFirstValue(state)).to.equal(3)
--- js/src/simulation/reducers/stats.js
+++ js/src/simulation/reducers/stats.js
@@ -1,5 +1,5 @@
 import { TICK } from '../../controls/actionCreators'
-import { DELIVER_BATCH } from '../actionCreators'
+import { RECEIVE_BATCH } from '../actionCreators'
 
 const initialState = {
   ticks: 0
@@ -8,7 +8,7 @@ const initialState = {
 const recordFirstValue = (state, action) => {
   if (state.ticksToFirstValue)
     return state
-  if (action.to !== 'customer')
+  if (action.workerName !== 'customer')
     return state
   return {
     ...state,
@@ -22,11 +22,11 @@ export default (simulationName) => (state=initialState, action) => {
       ...state,
       ticks: state.ticks + 1
     }
-  if (!action.simulationName || action.simulationName !== simulationName)
+  if (action.simulationName !== simulationName)
     return state
 
   switch (action.type) {
-    case DELIVER_BATCH:
+    case RECEIVE_BATCH:
       return recordFirstValue(state, action)
 
     default:
