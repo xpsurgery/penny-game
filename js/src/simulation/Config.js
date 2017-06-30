f87cd0bcb9b454b90820f07e53d351dea1c05da8
Kevin Rutherford <kevin@rutherford-software.com>
2017-06-30 15:15:43 +0100

    Display more config variables

--- js/src/simulation/Config.js
+++ js/src/simulation/Config.js
@@ -4,6 +4,8 @@ import NumericalStat from './NumericalStat'
 const Config = ({ simulation }) =>
   <div className='config'>
     <NumericalStat label='Batch size' value={simulation.s1.initialBatchSize} />
+    <NumericalStat label='Dev batch size' value={simulation.s3.currentBatchSize} />
+    <NumericalStat label='Dev task size' value={simulation.s3.taskTicks} />
   </div>
 
 export default Config
