be7b32598820c7da30552dc634dbfd0c2b369bae
Kevin Rutherford <kevin@rutherford-software.com>
2017-07-02 09:46:34 +0100

    Add icons to the control buttons

--- js/src/controls/Controls.js
+++ js/src/controls/Controls.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import FontAwesome from 'react-fontawesome'
 import { enableRepeater, disableRepeater } from './actionCreators'
 
 const Controls = ({ playEnabled, ticksSoFar, enableRepeater, disableRepeater }) =>
   <div className='controls group'>
     <div className='play-controls'>
-      <button disabled={playEnabled} onClick={enableRepeater}> play </button>
-      <button disabled={!playEnabled} onClick={disableRepeater}> pause </button>
+      <button disabled={playEnabled} onClick={enableRepeater}>
+        <FontAwesome name='play' />
+      </button>
+      <button disabled={!playEnabled} onClick={disableRepeater}>
+        <FontAwesome name='pause' />
+      </button>
     </div>
     <div className='ticks-so-far'>
       Ticks so far: {ticksSoFar}
