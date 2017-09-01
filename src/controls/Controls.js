12c59a3057529f967539be751ed2e1ed4ea259b1
Kevin Rutherford <kevin@rutherford-software.com>
2017-09-01 16:11:00 +0100

    Add tooltips to the controls

--- src/controls/Controls.js
+++ src/controls/Controls.js
@@ -2,21 +2,22 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import FontAwesome from 'react-fontawesome'
+import ReactTooltip from 'react-tooltip'
 import { enableRepeater, disableRepeater, resetAll } from './actionCreators'
 
 const Controls = ({ running, ticksSoFar, enableRepeater, disableRepeater, resetAll }) =>
   <div className='controls group'>
     <div className='play-controls'>
-      <button onClick={resetAll}>
+      <button onClick={resetAll} data-tip = 'Reset the simulation'>
         <FontAwesome name='fast-backward' />
       </button>
       {
         running ? (
-          <button onClick={disableRepeater}>
+          <button onClick={disableRepeater} data-tip ='Pause the simulation'>
             <FontAwesome name='pause' />
           </button>
         ) : (
-          <button onClick={enableRepeater}>
+          <button onClick={enableRepeater} data-tip='Run the simulation'>
             <FontAwesome name='play' />
           </button>
         )
@@ -25,6 +26,7 @@ const Controls = ({ running, ticksSoFar, enableRepeater, disableRepeater, resetA
     <div className='ticks-so-far'>
       Ticks so far: {ticksSoFar}
     </div>
+    <ReactTooltip type='light' effect='solid' border='true' />
   </div>
 
 Controls.displayName = 'Controls'
