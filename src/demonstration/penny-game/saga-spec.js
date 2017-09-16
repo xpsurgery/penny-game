0ec1f8c45794930aecf1e24cd8db4f4be9bd5900
Kevin Rutherford <kevin@rutherford-software.com>
2017-09-16 16:34:03 +0100

    Test the saga when the worker has nothing to do

--- src/demonstration/penny-game/saga-spec.js
+++ src/demonstration/penny-game/saga-spec.js
@@ -18,6 +18,31 @@ describe('Penny game saga', () => {
     taskTicks: 2
   })
 
+  describe('when the worker has nothing to do', () => {
+
+    describe('and this is the first worker in the line', () => {
+      it('tells the worker to get a new batch from the customer', () => {
+        let line = {
+          s1: reductio(reducer, [])
+        }
+        let acts = actions('test', line, 's1', 's2')
+        expect(acts.length).to.eq(1)
+        expect(acts[0].type).to.eq(newBatchFromCustomer().type)
+      })
+    })
+
+    describe('and the worker has a supplier', () => {
+      it('passes no instructions to the worker', () => {
+        let line = {
+          s2: reductio(reducer, [])
+        }
+        let acts = actions('test', line, 's2', 's3')
+        expect(acts.length).to.eq(0)
+      })
+    })
+
+  })
+
   describe('when the worker has a task in progress', () => {
     it('tells the worker to continue the task', () => {
       let line = {
