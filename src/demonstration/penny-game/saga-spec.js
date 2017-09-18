329bf67c603e0658da75ae8c7d4bb351577b36f6
Kevin Rutherford <kevin@rutherford-software.com>
2017-09-18 20:25:35 +0100

    Saga tests finished

--- src/demonstration/penny-game/saga-spec.js
+++ src/demonstration/penny-game/saga-spec.js
@@ -75,11 +75,8 @@ describe('Penny game saga', () => {
             continueTask('test', 's1'),
             continueTask('test', 's1')
           ]),
-          customer: {
-            currentBatchSize: 1
-          }
+          customer: reductio(reducer, [])
         }
-        console.log(line)
         let acts = actions('test', line, 's1', 'customer')
         expect(acts.length).to.eq(2)
         expect(acts[0].type).to.eq(deliverBatch().type)
@@ -90,9 +87,40 @@ describe('Penny game saga', () => {
     describe('and the next worker is not the customer', () => {
 
       describe('and is ready to receive', () => {
+        it('tells the worker to deliver the batch', () => {
+          let line = {
+            s1: reductio(reducer, [
+              receiveBatch('test', 's1', [{state: 'T'}]),
+              pickUpNextTask('test', 's1'),
+              continueTask('test', 's1'),
+              continueTask('test', 's1')
+            ]),
+            s2: reductio(reducer, [])
+          }
+          let acts = actions('test', line, 's1', 's2')
+          expect(acts.length).to.eq(2)
+          expect(acts[0].type).to.eq(deliverBatch().type)
+          expect(acts[1].type).to.eq(receiveBatch().type)
+        })
       })
 
       describe('and is not ready to receive', () => {
+        it('does nothing', () => {
+          let line = {
+            s2: reductio(reducer, [
+              receiveBatch('test', 's2', [{state: 'T'}]),
+              pickUpNextTask('test', 's2'),
+              continueTask('test', 's2'),
+              continueTask('test', 's2')
+            ]),
+            s3: reductio(reducer, [
+              receiveBatch('test', 's3', [{state: 'T'}]),
+              pickUpNextTask('test', 's3')
+            ])
+          }
+          let acts = actions('test', line, 's2', 's3')
+          expect(acts.length).to.eq(0)
+        })
       })
 
     })
